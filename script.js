70093683420a6d7b831847e42d590cd102c14a07
MarcellaMendoza <99848492+MarcellaMendoza@users.noreply.github.com>
2022-04-03 19:34:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function getLottoNumbers(){
+    const winningNums = []
+    for (let i = 0; i < 7; i++){
+        winningNums.push(Math.round(Math.random()*100))
+    }
+    return winningNums
+}
+
+function getWinningNumbersHTML() {
+  return getLottoNumbers().map(function(number) {
+    return `<div class="number">${number}</div>`
+  }) 
+}
+document.getElementById('winning-numbers').innerHTML = getWinningNumbersHTML()
